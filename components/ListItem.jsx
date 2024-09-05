cce2685849a9cce3f2fcd51e14d23fff0c7155c0
Wills <marleyenos18@gmail.com>
2024-09-05 18:06:31 -0500

    Delete and edit button position changed to bottom

--- components/ListItem.jsx
+++ components/ListItem.jsx
@@ -8,12 +8,15 @@ export default function ListItem({key, content, handleDelete, handleUpdate, date
     <div className="date-box">
          <li className='li-item' key={key}>
             <p>{content}</p>
-            <div className='list-icons'>
+            
+       </li>
+
+        
+        <div className='list-icons'>
+        <span className="date">{dateTime}</span>
                 <FaPencilAlt color='blue' size={15} onClick={handleUpdate} />
                 <FaTrash color='red' size={15} onClick={handleDelete} />
-            </div>
-       </li>
-        <span className="date">{dateTime}</span>
+        </div>
     </div>
   )
 }
